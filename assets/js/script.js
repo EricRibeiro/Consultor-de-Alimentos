fcca12cab8a34c844451fa826af12809eeda7aa9
Eric Ribeiro <ericribeiro@outlook.com.br>
2018-04-09 03:20:51 -0300

    Almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -368,6 +368,49 @@ function loadMaintainWeigthTable() {
     customizeTableComponents();
 }
 
+function loadMassGainTable() {
+    var table = "";
+    var content = "";
+
+    table += "<table id='aliments' class='table'>";
+    table += "<thead>";
+    table += "<tr>";
+    table += "<th class='text-center'>Descrição</th>";
+    table += "<th class='text-center'>Calorias</th>";
+    table += "<th class='text-center'>Proteína</th>";
+    table += "<th class='text-center'>Detalhes</th>";
+    table += "<th hidden>Calorias Sort</th>";
+    table += "<th hidden>Proteína Sort</th>";
+    table += "</thead>";
+    table += "<tbody>";
+    table += "</tbody>";
+    table += "</table>";
+
+    $('#aliments-table').html("");
+    $('#aliments-table').append(table);
+
+    for (var i = 0; i < aliments.length; i++) {
+        var proteina = aliments[i].proteina;
+        proteina = parseFloat(proteina);
+
+        if (proteina > 30 && !isNaN(proteina)) {
+            content += "<tr>";
+            content += "<td class='content-align'>" + aliments[i].descricao + "</td>";
+            content += "<td class='content-align'>" + evalAlimentsContent(aliments[i].energia.kcal, "kcal") + "</td>";
+            content += "<td class='content-align'>" + evalAlimentsContent(aliments[i].proteina, "g") + "</td>";
+            content += getBtnContent(i);
+            content += "<td hidden>" + evalAlimentsContentWithZero(aliments[i].energia.kcal) + "</td>";
+            content += "<td hidden>" + evalAlimentsContentWithZero(aliments[i].proteina) + "</td>";
+            content += "</tr>";
+        }
+    }
+
+    $('tbody').append(content);
+
+    initDataTables();
+    customizeTableComponents();
+}
+
 function bindSelectEvent() {
     $('#sel1').change(function () {
         $("#sel2").val("Selecionar Objetivo");
